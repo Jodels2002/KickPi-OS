7543a0cbe31a85eee74cf68fbd787a91131a50ab
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2021-01-07 08:54:19 +0100

    Update pimiga.sh

--- pimiga.sh
+++ pimiga.sh
@@ -30,6 +30,11 @@ make
 sudo make install
 sudo./compton -cCf -D 5
 
+cp -r /usr/share/lxdm/themes/Industrial ~
+cd
+mv Industrial Pimiga
+sudo ln -s Mein_Thema /usr/share/lxdm/themes
+
 sudo apt-get install -y libsdl2-2.0-0 libsdl2-ttf-2.0-0 libsdl2-image-2.0-0 libxml2 flac mpg123 libmpeg2-4
 sudo apt-get install -y libsdl2-dev libsdl2-ttf-dev libsdl2-image-dev libxml2-dev libflac-dev libmpg123-dev libpng-dev libmpeg2-4-dev
 sudo apt-get install -y libraspberrypi-dev
