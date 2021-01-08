0a8b128e0288d501c0072310a62c8d56c5f944de
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2021-01-08 08:55:38 +0100

    Add files via upload

---
+++ pimiga_select.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+# Install Pimiga
+# B.Titze 2020
+
+
+
+echo "Which Operating System do you like?"
+
+# Operating system names are used here as a data source
+select os in ClassicAmibian AmibianMate AmibianXFCE
+do
+if [ $os="ClassicAmibian" ];then
+updtate
+case $os in
+# Two case values are declared here for matching
+)
+echo "Lets keep Amibian as it is :-) "
+;;
+case $os in
+# Two case values are declared here for matching
+"AmibianMate")
+echo "Lets Amibian go "Mate"
+;;
+case $os in
+# Two case values are declared here for matching
+"AmibianMate")
+echo "Lets Amibian go XFCE"
+;;
+# Matching with invalid data
+*)
+echo "Invalid entry."
+break
+;;
+
+
+esac
+done
+
+echo " "
+echo " "
+echo " "
+
+echo "Pimiga3000 mini  Setup"
+echo " "
+echo " "
+echo "Lets start a upgrade.... sit down and relaxe...!"
+echo " "
+
