cf2aab3f7fa31fc6362cf342e8ff1d2292636f21
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2021-01-09 11:49:57 +0100

    Update pimiga.sh

--- pimiga.sh
+++ pimiga.sh
@@ -60,7 +60,7 @@ sudo apt-get -y dist-upgrade
 cd ~
 
 #Some little Tweaks....
-sudo apt-get -y install openbox-menu openbox-themes obconf obconf-qt obmenu libfm-extra libfm-extra libfm-extra
+sudo apt-get -y install openbox-menu openbox-themes obconf obconf-qt obmenu 
 sudo apt-get install -y libsdl2-2.0-0 libsdl2-ttf-2.0-0 libsdl2-image-2.0-0 libxml2 flac mpg123 libmpeg2-4
 sudo apt-get install -y libsdl2-dev libsdl2-ttf-dev libsdl2-image-dev libxml2-dev libflac-dev libmpg123-dev libpng-dev libmpeg2-4-dev
 sudo apt-get install -y libraspberrypi-dev
