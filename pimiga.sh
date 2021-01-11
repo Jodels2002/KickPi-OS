0e7788fd9ca97f92041f9ca2ae9daa5278af30de
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2021-01-11 20:15:04 +0100

    Update pimiga.sh

--- pimiga.sh
+++ pimiga.sh
@@ -47,20 +47,23 @@ mv /home/amibian/Pimiga_mini/.data/.config/lxterminal ~/.config/lxterminal
 
 echo "XServer re-install.....# if needed :) "
 
-#sudo apt-get -y remove --auto-remove xserver-common*
-#sudo apt-get -y sudo apt-get purge openbox lxde
+
+sudo apt-get -y remove --auto-remove xserver-common*
+sudo apt-get -y sudo apt-get purge openbox lxde
 
 echo "Time to update:) "
 sudo apt-get -y update 
-#sudo apt-get -y upgrade
-sudo apt-get -y dist-upgrade
+sudo apt-get -y upgrade
+
+sudo apt install -y xserver-xorg xfce4 xfce4-goodies
+sudo dpkg-reconfigure lightd
 
 #restore.....#  if needed:) "
 #sudo cp -R  /home/amibian/.backup/ usr/local/bin
 cd ~
 
 #Some little Tweaks....
-sudo apt-get -y install openbox-menu openbox-themes obconf obconf-qt obmenu 
+#sudo apt-get -y install openbox-menu openbox-themes obconf obconf-qt obmenu 
 sudo apt-get install -y libsdl2-2.0-0 libsdl2-ttf-2.0-0 libsdl2-image-2.0-0 libxml2 flac mpg123 libmpeg2-4
 sudo apt-get install -y libsdl2-dev libsdl2-ttf-dev libsdl2-image-dev libxml2-dev libflac-dev libmpg123-dev libpng-dev libmpeg2-4-dev
 sudo apt-get install -y libraspberrypi-dev
