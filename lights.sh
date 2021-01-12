aad811ee311b42e9d360d71ad7735580ce8539d8
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2021-01-12 10:44:11 +0100

    Add files via upload

---
+++ lights.sh
@@ -0,0 +1,118 @@
+#!/bin/bash
+
+# LED Control PiMiga 3000 B.Titze...
+# ...to be finished :-)...möööp
+
+BASE_GPIO_PATH=/sys/class/gpio
+
+# Assign LEDs & definitions
+BLUE=9
+GREEN=10
+YELLOW=11
+ORANGE=12
+RED=13
+ON="1"
+OFF="0"
+
+# exportPin()
+{
+  if [ ! -e $BASE_GPIO_PATH/gpio$1 ]; then
+    echo "$1" > $BASE_GPIO_PATH/export
+  fi
+}
+setOutput()
+{
+  echo "out" > $BASE_GPIO_PATH/gpio$1/direction
+}
+
+# Function set Lights
+LightState()
+{
+  echo $2 > $BASE_GPIO_PATH/gpio$1/value
+}
+
+# Function lights off
+allLightsOff()
+{
+  LightState $BLUE $OFF
+  LightState $GREEN $OFF
+  LightState $YELLOW $OFF
+  LightState $ORANGE $OFF
+  LightState $RED $OFF
+}
+
+# Export pins so that we can use them
+exportPin $BLUE
+exportPin $YELLOW
+exportPin $RED
+exportPin $ORANGE
+exportPin $RED
+
+# Set pins as outputs
+setOutput $BLUE
+setOutput $GREEN
+setOutput $YELLOW
+setOutput $ORANGE
+setOutput $RED
+
+# Shutdown... light´s out
+shutdown()
+{
+  allLightsOff
+  exit 0
+}
+
+trap shutdown SIGINT
+
+# Turn lights off to begin
+allLightsOff
+
+# Main Loop forever...
+while [ 1 ]
+do
+  # Do what you want.... Seq1
+  setLightState $BLUE $ON
+  setLightState $GREEN $ON
+  setLightState $YELLOW $ON
+  setLightState $ORANGE $ON
+  setLightState $RED $ON
+  sleep 3
+
+  # Do what you want.... Seq2
+  setLightState $BLUE $ON
+  setLightState $GREEN $OFF
+  setLightState $YELLOW $OFF
+  setLightState $ORANGE $OFF
+  setLightState $RED $OFF
+  sleep 1
+
+  # Do what you want.... Seq3
+  setLightState $BLUE $ON
+  setLightState $GREEN $ON
+  setLightState $YELLOW $OFF
+  setLightState $ORANGE $OFF
+  setLightState $RED $OFF
+  sleep 5
+ 
+ # Do what you want.... Seq4
+  setLightState $BLUE $ON
+  setLightState $GREEN $ON
+  setLightState $YELLOW $ON
+  setLightState $ORANGE $OFF
+  setLightState $RED $OFF
+  sleep 2
+
+  # Do what you want.... Seq5
+  setLightState $BLUE $ON
+  setLightState $GREEN $ON
+  setLightState $YELLOW $ON
+  setLightState $ORANGE $ON
+  setLightState $RED $OFF
+  
+  # Do what you want.... Seq6
+  setLightState $BLUE $ON
+  setLightState $GREEN $ON
+  setLightState $YELLOW $ON
+  setLightState $ORANGE $ON
+  setLightState $RED $ON
+done
