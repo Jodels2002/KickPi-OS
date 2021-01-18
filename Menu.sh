72b534a9c540d508abb7aa9b20c5ee0ebd2daa9c
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2021-01-18 12:17:56 +0100

    Create Menu.sh

---
+++ Menu.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+HEIGHT=15
+WIDTH=40
+CHOICE_HEIGHT=4
+BACKTITLE="Pimiga3000"
+TITLE="Title here"
+MENU="Please hoose one:"
+
+OPTIONS=(1 "Install Pimiga3000"
+         2 "Install Pimiga3000, Retropie & Tools"
+         3 "Install Pimiga3000, Pimiga3000 Desktop, Retropie & Tools")
+
+CHOICE=$(dialog --clear \
+                --backtitle "$Pimiga3000" \
+                --title "$TITLE" \
+                --menu "$MENU" \
+                $HEIGHT $WIDTH $CHOICE_HEIGHT \
+                "${OPTIONS[@]}" \
+                2>&1 >/dev/tty)
+
+clear
+case $CHOICE in
+        1)
+            echo "Pimiga3000"
+            ;;
+        2)
+            echo "Pimiga3000, Retropie & Tools"
+            ;;
+        3)
+            echo "Pimiga3000, Pimiga3000 Desktop, Retropie & Tools"
+            ;;
+esac
