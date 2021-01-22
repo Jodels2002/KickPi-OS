9006aadcbe035217336662652b9dd029b7b40337
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2021-01-22 09:33:18 +0100

    Create oled_install.sh

---
+++ OLED/oled_install.sh
@@ -0,0 +1,12 @@
+
+echo "Enable the I2C Interface on the Raspberry Pi"
+
+sudo raspi-config
+
+
+sudo apt install -y python3-dev
+sudo apt install -y python-smbus i2c-tools
+sudo apt install -y python3-pil
+sudo apt install -y python3-pip
+sudo apt install -y python3-setuptools
+sudo apt install -y python3-rpi.gpio
