dbbaefd1158324ae494b342a2423a0dda9a67a78
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2021-01-25 10:08:11 +0100

    Update Amiga.py

--- OLED/Amiga.py
+++ OLED/Amiga.py
@@ -25,22 +25,22 @@ x = padding
 
 
 font = ImageFont.load_default()
-font = ImageFont.truetype('FreeSerif.ttf', 18)
-
-draw.text((x+10, top),    'Pimiga',  font=font, fill=1)
-draw.text((x+10, top+40), ' 3000', font=font, fill=1)
-oled.display()
-sleep(3)
-
-
-
-font = ImageFont.load_default()
 font = ImageFont.truetype('FreeSerif.ttf', 24)
 
 draw.text((x+10, top),    'Pimiga',  font=font, fill=1)
 draw.text((x+10, top+40), ' 3000', font=font, fill=1)
 oled.display()
-sleep(3)
+sleep(10)
+
+oled.cls() 
+
+font = ImageFont.load_default()
+font = ImageFont.truetype('FreeSerif.ttf', 24)
+
+draw.text((x+10, top),    'Proceed',  font=font, fill=1)
+draw.text((x+10, top+40), ' install...', font=font, fill=1)
+oled.display()
+sleep(10)
 
 #draw.rectangle((0, 0, oled.width-1, oled.height-1), outline=255, fill=1)
 oled.cls() 
@@ -48,6 +48,6 @@ sleep(1)
 logo = Image.open('pi_logo.png')
 draw.bitmap((32, 0), logo, fill=1)
 oled.display()
-sleep(3)
+sleep(5000)
 
 oled.cls()      # Oled still on, but screen contents now blacked out
