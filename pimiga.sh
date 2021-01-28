efc9d6b993938fa3be8f29370c20ff2fd5f55f15
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2021-01-28 19:49:59 +0000

    Update pimiga.sh

--- pimiga.sh
+++ pimiga.sh
@@ -241,7 +241,7 @@ echo " "
       sudo apt-get install -y libfreetype6-dev libgl1-mesa-dev libgles2-mesa-dev libdrm-dev libgbm-dev libudev-dev libasound2-dev liblzma-dev libjpeg-dev libtiff-dev libwebp-dev git build-essential
       sudo apt-get install -y gir1.2-ibus-1.0 libdbus-1-dev libegl1-mesa-dev libibus-1.0-5 libibus-1.0-dev libice-dev libsm-dev libsndio-dev libwayland-bin libwayland-dev libxi-dev libxinerama-dev libxkbcommon-dev libxrandr-dev libxss-dev libxt-dev libxv-dev x11proto-randr-dev x11proto-scrnsaver-dev x11proto-video-dev x11proto-xinerama-dev
       
-      sudo apt install -y  geany geany-plugins-common geany-common imagemagick gparted synaptic chromium-browser krita-l10n synaptic mc
+      sudo apt install -y  geany geany-plugins-common geany-common imagemagick gparted synaptic chromium-browser krita-l10n synaptic mc zip
       
       
       
