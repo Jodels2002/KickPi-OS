a39e250135baedf931d6004c04b71ca93b37f7f6
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2021-02-04 09:40:54 +0100

    Update retro-term.sh

--- config/retro-term.sh
+++ config/retro-term.sh
@@ -4,9 +4,9 @@
 # Author      : Jose Cerrejon Gonzalez (ulysess@gmail_dot._com)
 # Version     : 1.0.0 (13/Aug/20)
 #
-. ./scripts/helper.sh || . ../helper.sh || . ./helper.sh || wget -q 'https://github.com/jmcerrejon/PiKISS/raw/master/scripts/helper.sh'
+
 clear
-check_board || { echo "Missing file helper.sh. I've tried to download it for you. Try to run the script again." && exit 1; }
+
 
 PACKAGES_DEV=(qmlscene qt5-qmake qt5-default qtdeclarative5-dev qml-module-qtquick-controls qml-module-qtgraphicaleffects qml-module-qtquick-dialogs qml-module-qtquick-localstorage qml-module-qtquick-window2 qml-module-qt-labs-settings qml-module-qt-labs-folderlistmodel)
 GITHUB_PATH="https://github.com/Swordfish90/cool-retro-term.git"
@@ -19,7 +19,7 @@ runme() {
 		echo -e "\nFile does not exist.\n· Something is wrong.\n· Try to install again."
 		exit_message
 	fi
-	read -p "Press [ENTER] to run the app..."
+	
 	cool-retro-term
 	exit_message
 }
@@ -41,22 +41,11 @@ compile() {
     qmake && make -j"$(nproc)" OPTOPT="-march=armv8-a+crc -mtune=cortex-a53"
     sudo make install
     fix_icon
-    echo -e "\nDone!. Type cool-retro-term or go to Menu > System tools > Cool Retro Term."
+  
     runme
 }
 
-echo "Compile cool-retro-term based on QT5"
-echo "===================================="
-echo
-echo " · Estimated Time on Raspberry Pi 4 (not overclocked): ~2 minutes"
-echo " · It's very cool, but I warn you: Pkg dependencies takes ~54 MB and the app in memory takes ~88 MB RAM."
-echo " · This app can't be uninstalled by PiKISS if you decide to run this script. Use at your own risk."
-echo " · [ALT] + F11 = Full Screen | Right click = Menu"
-echo
-read -p "Continue (Y/n)? " response
 
-if [[ $response =~ [Nn] ]]; then
-	exit_message
-fi
+
 
 compile
