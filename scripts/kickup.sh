82b1f6d909c0d124082707717db56f6a691d3de0
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2021-02-08 23:36:11 +0100

    Update kickup.sh

--- scripts/kickup.sh
+++ scripts/kickup.sh
@@ -6,7 +6,7 @@
 sudo rm -rf /home/$USER/KickPi-OS/
 sudo rm -rf /home/$USER/.KickPi-OS/
 cd
-
+sudo apt -y upgrade
 git clone --depth=1 https://github.com/Jodels2002/KickPi-OS.git
 
 sudo chmod -R 777 KickPi-OS
