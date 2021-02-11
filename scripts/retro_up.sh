c4760d27c20fcd93d00f280062ca7efa179e7d64
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2021-02-11 08:37:07 +0100

    Create retro_up.sh

---
+++ scripts/retro_up.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#***********************************************  #KickPi-OS Retropie content loader  ***********************************
+# Install KickPi-OS
+# B.Titze 2021
+#***********************************************  #Some Info for the future  ***********************************
+
+#https://archive.org/compress/Amiga_WHD_Games ;-) 
+#Todo
+#https://archive.org/compress/2020_01_06_fbn/formats=ZIP&file=/2020_01_06_fbn.zip
+#https://archive.org/compress/MAME2003_Reference_Set_MAME0.78_ROMs_CHDs_Samples/formats=ZIP&file=/MAME2003_Reference_Set_MAME0.78_ROMs_CHDs_Samples.zip
+#https://archive.org/download/archive_20190819/Romset.zip
+#https://archive.org/download/retroarch_bios_pack/extract_to_sytem_folder.zip
+
+cd RetroPi
+
+wget https://archive.org/compress/2020_01_06_fbn/formats=ZIP&file=/2020_01_06_fbn.zip
+wget https://archive.org/compress/MAME2003_Reference_Set_MAME0.78_ROMs_CHDs_Samples/formats=ZIP&file=/MAME2003_Reference_Set_MAME0.78_ROMs_CHDs_Samples.zip
+wget https://archive.org/compress/Amiga_WHD_Games
