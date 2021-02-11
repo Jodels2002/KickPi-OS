838cac7eb9c59d98c0985dc091c9acd11d5d4215
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2021-02-11 09:03:22 +0100

    Update retro_up.sh

--- scripts/retro_up.sh
+++ scripts/retro_up.sh
@@ -16,3 +16,4 @@ cd RetroPi
 wget https://archive.org/compress/2020_01_06_fbn/formats=ZIP&file=/2020_01_06_fbn.zip
 wget https://archive.org/compress/MAME2003_Reference_Set_MAME0.78_ROMs_CHDs_Samples/formats=ZIP&file=/MAME2003_Reference_Set_MAME0.78_ROMs_CHDs_Samples.zip
 wget https://archive.org/compress/Amiga_WHD_Games
+weget https://archive.org/compress/commodore-64-romset-us/formats=ZIP&file=/commodore-64-romset-us.zip
