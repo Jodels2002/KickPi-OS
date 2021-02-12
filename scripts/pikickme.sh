9b489369a96b8a4d09b0efdca735bbebbd65fab4
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2021-02-12 12:03:40 +0000

    Update pikickme.sh

--- scripts/pikickme.sh
+++ scripts/pikickme.sh
@@ -689,12 +689,12 @@ if [ ! -f "/opt/retropie/supplementary/emulationstation/emulationstation" ]; the
       toilet "KickPi-OS" --metal
       sudo git clone --recursive --depth 1 --branch master "https://github.com/RetroHursty69/es-theme-magazinemadness.git" "/etc/emulationstation/themes/magazinemadness"
       
-      cd /home/$USER/KickPi-OS/Retropie/
+      d /home/$USER/KickPi-OS/Retropie/
       unzip -u /home/$USER/KickPi-OS/Retropie/roms.zip
       cp -rf /home/$USER/KickPi-OS/Retropie/roms/* /home/$USER/RetroPie/roms
       unzip -u /home/$USER/KickPi-OS/Retropie/all.zip
       sudo cp -R /home/$USER/KickPi-OS/Retropie/all/* /opt/retropie/configs/
-      cp $HOME/games/vice/IMAGES/prg/* /home/$USER/RetroPie/roms/c64/   
+      cp $HOME/games/vice/IMAGES/prg/* /home/$USER/RetroPie/roms/c64/    
       
       if [ ! -f /home/$USER/RetroPie/roms/Romset.zip ]; then
       clear
@@ -730,7 +730,7 @@ if [ ! -f "/opt/retropie/supplementary/emulationstation/emulationstation" ]; the
       clear
       
       fi
-      
+      sudo cp -rf /home/$USER/KickPi-OS/Retropie/es_settings.cfg /opt/retropie/configs/emulationstation/
       sudo chmod -R 777 /home/$USER/RetroPie/
       sudo chmod -R 777 /home/$USER/RetroPie-Setup/
       sudo chmod -R 777 /opt/retropie/
