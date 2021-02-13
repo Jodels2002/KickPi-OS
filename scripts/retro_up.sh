7c964c8ab9d07b33ea2ae0572e022b2f6bb29e49
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2021-02-13 13:24:41 +0100

    Update retro_up.sh

--- scripts/retro_up.sh
+++ scripts/retro_up.sh
@@ -5,22 +5,7 @@
 #***********************************************  #Some Info for the future  ***********************************
 # not ready....
 
-if [ ! -f /home/$USER/RetroPie/roms/Romset.zip ]; then
-      clear
-      toilet -F gay Retropie
-      echo " "
-      echo " "
-      echo "  Configure Retropie Romset from Archiv.org " 
-      echo " "
-      echo " "
-      cd /home/$USER/RetroPie/roms
-      wget https://archive.org/download/archive_20190819/Romset.zip
-      unzip -u /home/$USER/RetroPie/roms/Romset.zip
-      
-      else 
-      clear
-      
-      fi
+
       
 if [ ! -f /home/$USER/RetroPie/roms/Amiga_WHD_Games ]; then
       clear
