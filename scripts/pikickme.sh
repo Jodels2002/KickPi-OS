ccb49608a3e514018431c1a4667cf399e5954f28
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2021-02-15 16:03:22 +0000

    Update pikickme.sh

--- scripts/pikickme.sh
+++ scripts/pikickme.sh
@@ -370,14 +370,19 @@ fi
     else
     
        echo " ... here comes Amiberry 32 bit   :-)"
-      
-   
-	  cd /home/$USER/KickPi-OS/Amiga
+       if [ ! -f /home/$USER/Amiga/amiberry]; then
+          cd /home/$USER/KickPi-OS/Amiga
       	  unzip -u /home/$USER/KickPi-OS/Amiga/amiberry-v3.3-rpi4-dmx-32bit.zip 
           cp -rf /home/$USER/KickPi-OS/Amiga/amiberry-rpi4-dmx-32bit/* /home/$USER/Amiga/
+      else 
+      clear
+      
+      fi
+   
+	  
 	  
        
-       
+   
       
     fi
     
