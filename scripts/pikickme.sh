6aae8ad886cd25c32d8ce440507ed0bb9ab4574c
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2021-02-15 18:59:42 +0000

    Update pikickme.sh

--- scripts/pikickme.sh
+++ scripts/pikickme.sh
@@ -388,16 +388,15 @@ fi
        
         
          clear
-          echo " ... Amiberry found   :-)"
-	  echo " ... Amiberry found   :-)"
+	  echo "  ... here comes Amiberry 32 bit :-) "
+          d /home/$USER/KickPi-OS/Amiga
+      	  unzip -u /home/$USER/KickPi-OS/Amiga/amiberry-v3.3-rpi4-dmx-32bit.zip 
+          cp -rf /home/$USER/KickPi-OS/Amiga/amiberry-rpi4-dmx-32bit/* /home/$USER/Amiga/
       else 
         echo " ... here comes Amiberry 32 bit  false"
           clear
-	  cd /home/$USER/KickPi-OS/Amiga
-      	  unzip -u /home/$USER/KickPi-OS/Amiga/amiberry-v3.3-rpi4-dmx-32bit.zip 
-          cp -rf /home/$USER/KickPi-OS/Amiga/amiberry-rpi4-dmx-32bit/* /home/$USER/Amiga/
-	 echo " ... Amiberry installed   :-)"
-      fi
+	  
+	 fi
 
       
     fi
