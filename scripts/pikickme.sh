c656818de9f2de5869d532db47f2209c418bdba9
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2021-02-16 06:03:39 +0000

    Update pikickme.sh

--- scripts/pikickme.sh
+++ scripts/pikickme.sh
@@ -187,6 +187,7 @@ KickPi-OS_Tools() {
       unzip -u  /home/$USER/KickPi-OS/.pac/.worker.zip
       cp -rf /home/$USER/KickPi-OS/.pac/.worker/.worker.zip /home/$USER/
       cp -rf /home/$USER/KickPi-OS/.pac/.worker /home/$USER/
+      sudo chmod -R 777 /home/$USER/.worker/
       
       else 
       clear
