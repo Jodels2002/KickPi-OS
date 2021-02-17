cb2e39cddae98fa8693a5e888d742f825ce05216
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2021-02-17 09:48:48 +0100

    Update kickup.sh

--- scripts/kickup.sh
+++ scripts/kickup.sh
@@ -2,6 +2,9 @@
 # Install KickPi-OS
 # B.Titze 2021
 # KickPi-OS updater
+wget -q --spider http://google.com
+
+if [ $? -eq 0 ]; then
 
 sudo rm -rf /home/$USER/KickPi-OS/
 sudo rm -rf /home/$USER/.KickPi-OS/
@@ -12,3 +15,6 @@ git clone --depth=1 https://github.com/Jodels2002/KickPi-OS.git
 sudo chmod -R 777 KickPi-OS
 
 /home/$USER/KickPi-OS/scripts/pikickme.sh
+else
+    whiptail --msgbox " You are not connected to the Internet." 20 50 1
+fi
