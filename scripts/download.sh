de6b3694066964ac1f9f248b27eefec8bfe5eff4
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2021-02-18 14:19:35 +0000

    Add files via upload

---
+++ scripts/download.sh
@@ -0,0 +1,3 @@
+sudo apt install -y games-finest gnome-twitch gnome-twitch-player-backend-gstreamer-clutter gnome-twitch-player-backend-gstreamer-cairo gnome-twitch-player-backend-gstreamer-opengl
+sudo apt install -y gnome-twitch-player-backend-mpv-opengl gnome-video-arcade gnome-video-effects handbrake kaffeine libavcodec-extra58
+sudo apt install -y pcsxr qmmp quicktime-utils radio videocut xserver-xorg-video-all youtube-dl minitube transmission
