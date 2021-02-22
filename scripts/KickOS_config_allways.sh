cd0be71a8f06e692f663e05c4b3e0ef205312771
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2021-02-22 11:42:58 +0100

    Update KickOS_config_allways.sh

--- scripts/KickOS_config_allways.sh
+++ scripts/KickOS_config_allways.sh
@@ -1,3 +1,15 @@
-sudo systemctl daemon-reload
-sudo systemctl start KickPi-OS_config_allways
-sudo systemctl enable KickPi-OS_config_allways
+if [ ! -f /etc/systemd/system/KickPi-OS_config_allways.service ]; then
+      # First installation
+      clear
+      
+      sudo systemctl daemon-reload
+      sudo systemctl start KickPi-OS_config_allways
+      sudo systemctl enable KickPi-OS_config_allways
+      else 
+      clear
+      # Configured
+      
+      fi
+
+
+
