2d746673b0a81a2a23204978759775a0f960234b
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2021-02-23 11:48:12 +0100

    Update pikickme.sh

--- scripts/pikickme.sh
+++ scripts/pikickme.sh
@@ -788,8 +788,8 @@ if [ ! -f "/home/$USER/RetroPie-Setup/retropie_packages.sh" ]; then
       sudo unzip -u /home/$USER/KickPi-OS/Retropie/data.zip
       clear
       toilet "KickPi-OS" --metal
-      #sudo chmod -R 777  /home/$USER/KickPi-OS/Retropie/
-      #cp -rf /home/$USER/KickPi-OS/Retropie/roms/* /home/$USER/RetroPie/roms
+      sudo chmod -R 777  /home/$USER/KickPi-OS/Retropie/
+      cp -rf /home/$USER/KickPi-OS/Retropie/roms/* /home/$USER/RetroPie/roms
       toilet -F gay NOTE!
       echo " "
       echo " "
