e088c69827226d14a8143c65629c2e055ca5ace1
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2021-02-24 14:14:24 +0100

    Update pikickme.sh

--- scripts/pikickme.sh
+++ scripts/pikickme.sh
@@ -188,7 +188,7 @@ KickPi-OS_Tools() {
      
       sudo apt install -y geany geany-plugins-common geany-common xmlstarlet
       sudo apt install -y mc zip unzip  
-      sudo apt install -y omxplayer chromium-codecs-ffmpeg    
+       
       clear
       toilet "KickPi-OS" --metal
       
@@ -508,8 +508,8 @@ fi
        echo " "
        echo " "
        echo " ... here comes Amiberry 64 bit   :-)"
-       sudo apt-get install -y libsdl2-2.0-0 libsdl2-ttf-2.0-0 libsdl2-image-2.0-0 flac mpg123 libmpeg2-4
-       sudo apt-get install -y libsdl2-dev libsdl2-ttf-dev libsdl2-image-dev libflac-dev libmpg123-dev libpng-dev libmpeg2-4-dev
+       #sudo apt-get install -y libsdl2-2.0-0 libsdl2-ttf-2.0-0 libsdl2-image-2.0-0 flac mpg123 libmpeg2-4
+       #sudo apt-get install -y libsdl2-dev libsdl2-ttf-dev libsdl2-image-dev libflac-dev libmpg123-dev libpng-dev libmpeg2-4-dev
        clear
        toilet "KickPi-OS" --metal
        toilet -F gay 64bit
@@ -749,8 +749,9 @@ KickPi-OS_Retropie() {
      git clone --depth=1 https://github.com/RetroPie/RetroPie-Setup.git
      sudo chmod -R 777 /home/$USER/RetroPie-Setup/
   else 
+  
       sudo apt-get -y install libsdl2-dev libsdl2-ttf-dev libsdl2-image-dev libxml2-dev libflac-dev libmpg123-dev libpng-dev libmpeg2-4-dev
-
+      sudo apt install -y omxplayer chromium-codecs-ffmpeg   
       	if [ ! -f "/home/$USER/RetroPie-Setup/retropie_packages.sh" ]; then     
       	echo " "
       	clear
