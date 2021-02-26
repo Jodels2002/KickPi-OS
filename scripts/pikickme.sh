334254618767bbb4b07b403668854ede369ddd01
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2021-02-26 08:03:09 +0100

    Update pikickme.sh

--- scripts/pikickme.sh
+++ scripts/pikickme.sh
@@ -11,7 +11,9 @@
 #16.02 add TwisertOS, Bugfix Update optimiced, RetroPie Bugfix (Autoconfig Amiberry)  Bugfix 64 bit, Worker preconfigured / OK
 #16.02 Bugfix Reboot, Speedup
 #20.02 Photogimp, Autoboot to Amiberry
-#20.02 Bootmenu, Speedup Boot time  ->sudo update-rc.d motd remove
+#20.02 Bootmenu, Speedup Boot time  ->
+# sudo update-rc.d motd remove
+# ExecStart=-/sbin/agetty --skip-login --noclear --noissue --login-options "-f pi" %I $TERM
 #Amiberry64=amiberry-v3.3-rpi4-64bit.zip
 
 #***********************************************  #Are you runing Desktop?  ***********************************
@@ -336,7 +338,7 @@ KickPi-OS_Games() {
       sudo apt purge -y lxde  lxde-common lxde-core openbox-lxde-session
       sudo apt purge -y raspberrypi-ui-mods 
       sudo apt purge -y cups cups-client cups-common cups-server-common
-      sudo update-rc.d motd remove
+      # sudo update-rc.d motd remove
       sudo apt install -y xserver-xorg xfce4 xfce4-goodies lxinput
       
   # Preconfigure Silent Boot
