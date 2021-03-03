5096568e3fca0eea9fc169a6819b54a79abc08c1
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2021-03-03 11:45:10 +0100

    Update oled.sh

--- scripts/oled.sh
+++ scripts/oled.sh
@@ -2,7 +2,7 @@
 #***********************************************  #KickPi-OS install script  ***********************************
 # Install KickPi-OS
 # B.Titze 2021
-#***********************************************  #install OLED Display  ***********************************
+#***********************************************  #install BW OLED Display  ***********************************
 # https://www.raspberrypi-spy.co.uk/2018/04/i2c-oled-display-module-with-raspberry-pi/
 
 # Service Einrichten! https://www.thomaschristlieb.de/ein-python-script-mit-systemd-als-daemon-systemd-tut-garnicht-weh/
@@ -31,3 +31,18 @@ sudo chmod -R 777 /home/$USER/.KickPi-OS/OLED
 #i2cdetect -y 1
 #echo " Should Say: "3C""
 python Amiga.py
+
+#!/bin/bash
+#***********************************************  #KickPi-OS install script  ***********************************
+# Install KickPi-OS
+# B.Titze 2021
+# https://luma-oled.readthedocs.io/en/latest/python-usage.html
+#***********************************************  #install colour OLED Display  ***********************************
+
+sudo -H pip3 install --upgrade luma.oled
+sudo apt-get update
+sudo apt-get install python3 python3-pip python3-pil libjpeg-dev zlib1g-dev libfreetype6-dev liblcms2-dev libopenjp2-7 libtiff5 -y
+sudo -H pip3 install luma.oled
+sudo usermod -a -G spi,gpio,i2c pi
+
+
