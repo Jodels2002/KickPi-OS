9026ec31673b7d77d312a57f6434a424e2e69897
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2021-03-11 09:05:12 +0100

    Update fritz.sh

--- scripts/fritz.sh
+++ scripts/fritz.sh
@@ -1,10 +1,10 @@
-sudo -i
-apt-get update
-apt-get install libboost-all-dev build-essential git-core cmake libssl-dev libudev-dev qt5-default libqt5serialport5-dev libqt5svg5-dev
-apt-get install -t testing libgit2-dev
+
+sudo apt-get update
+sudo apt-get install -y libboost-all-dev build-essential git-core cmake libssl-dev libudev-dev qt5-default libqt5serialport5-dev libqt5svg5-dev
+sudo apt-get install -y -t testing libgit2-dev
 
 
-cd /usr/src/
+cd 
 git clone https://github.com/fritzing/fritzing-app.git
 git clone https://github.com/fritzing/fritzing-parts.git
 cd fritzing-app
