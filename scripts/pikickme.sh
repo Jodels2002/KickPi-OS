f90811e511e91f36c1cf5c4a77e65eec23b44427
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2021-03-16 12:59:53 +0100

    Update pikickme.sh

--- scripts/pikickme.sh
+++ scripts/pikickme.sh
@@ -218,7 +218,7 @@ KickPi-OS_Tools() {
       sudo apt install -y geany geany-plugins-common geany-common xmlstarlet
       sudo apt-get install -y libsdl2-2.0-0 libsdl2-ttf-2.0-0 libsdl2-image-2.0-0 libxml2 flac mpg123 libmpeg2-4    
       sudo apt-get -y install libsdl2-dev libsdl2-ttf-dev libsdl2-image-dev libxml2-dev libflac-dev libmpg123-dev libpng-dev libmpeg2-4-dev
-   
+      wget -qO- https://raw.githubusercontent.com/Botspot/pi-apps/master/install | bash
        
        
       clear
