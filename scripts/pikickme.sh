91c675592bdab591e4c8fc8363d73482c67ec9b1
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2021-03-16 13:55:13 +0100

    Update pikickme.sh

--- scripts/pikickme.sh
+++ scripts/pikickme.sh
@@ -840,6 +840,20 @@ KickPi-OS_Retropie() {
 	sudo chmod -R 777 /opt/retropie/emulators/amiberry/
 	sudo chmod -R 777 /opt/retropie/configs/amiga
 	
+	# C64
+	sudo apt install -y autoconf automake build-essential byacc dos2unix flex libavcodec-dev libavformat-dev libgtk2.0-cil-dev libgtkglext1-dev libmp3lame-dev libmpg123-dev libpcap-dev libpulse-dev libreadline-dev libswscale-dev libvte-dev libxaw7-dev subversion yasm libgtk3.0-cil-dev xa65 libsdl2-dev libsdl2-image-dev libgtk-3-dev libglew-dev
+
+       mkdir -p src
+       cd src
+       svn checkout https://svn.code.sf.net/p/vice-emu/code/trunk trunk
+       cd trunk/vice
+       ./autogen.sh
+       ./configure --disable-pdf-docs --without-pulse
+       make -j4
+       mkdir $HOME/games
+       cd $HOME/games
+       #sudo cp -rf /home/$USER/KickPi-OS/Retropie/es_systems.cfg /etc/emulationstation/
+sudo make install
      else
      clear
      toilet "KickPi-OS" --metal
