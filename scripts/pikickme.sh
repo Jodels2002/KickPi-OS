d797498a5cfb95afde11781a40b7ecddee3c76f3
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2021-03-17 16:21:13 +0000

    Update pikickme.sh

--- scripts/pikickme.sh
+++ scripts/pikickme.sh
@@ -12,7 +12,7 @@
 #16.02 Bugfix Reboot, Speedup
 #20.02 Photogimp, Autoboot to Amiberry
 #20.02 Bootmenu, Speedup Boot time  ->
-# 64 bit, pi-apps
+# 64 bitRetropie Amiberry, pi-apps, Vice64
 # sudo update-rc.d motd remove
 # consoleblank=0  /boot/cmdline.txt
 # ExecStart=-/sbin/agetty --skip-login --noclear --noissue --login-options "-f pi" %I $TERM
