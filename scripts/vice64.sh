75479f7c1584e64f8f3b25ad1d0ecde2379baab0
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2021-03-17 11:44:49 +0000

    Update vice64.sh

--- scripts/vice64.sh
+++ scripts/vice64.sh
@@ -1,4 +1,5 @@
 sudo apt install -y autoconf automake build-essential byacc dos2unix flex libavcodec-dev libavformat-dev libgtk2.0-cil-dev libgtkglext1-dev libmp3lame-dev libmpg123-dev libpcap-dev libpulse-dev libreadline-dev libswscale-dev libvte-dev libxaw7-dev subversion yasm libgtk3.0-cil-dev xa65 libsdl2-dev libsdl2-image-dev libgtk-3-dev libglew-dev
+
 cd
 mkdir -p src
 cd src
@@ -8,6 +9,9 @@ cd trunk/vice
 ./configure --disable-pdf-docs --without-pulse
 make -j4
 sudo make install
+cd
+mkdir /home/$USER/games
+mkdir /home/$USER/games/vice/
 sudo cp -rf /usr/local/share/vice/ $HOME/games
 sudo cp -rf /usr/local/bin/x* $HOME/games/vice/
 sudo cp -rf /usr/local/bin/x64sc $HOME/games/vice/x64
