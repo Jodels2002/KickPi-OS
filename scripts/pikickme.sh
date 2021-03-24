e6b38400d4b1529ab958b1c66736b754136fd781
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2021-03-24 16:52:55 +0000

    Update pikickme.sh

--- scripts/pikickme.sh
+++ scripts/pikickme.sh
@@ -540,12 +540,12 @@ if [ "$(getconf LONG_BIT)" == "64" ]; then
        echo " "
        if [ ! -f /home/$USER/.config/xfce4/panel/panel.zip ]; then
        
-          sudo rm /home/$USER/.config/xfce4/panel/
-	  mkdir /home/$USER/.config/xfce4/panel/
-          cd /home/$USER/KickPi-OS/config/
-      	  unzip -u /home/$USER/KickPi-OS/config/panel.zip
-	  cp -rf /home/$USER/KickPi-OS/config/panel/* /home/$USER/.config/xfce4/panel/
-	  cp -rf /home/$USER/KickPi-OS/config/panel.zip /home/$USER/.config/xfce4/panel/
+          #sudo rm /home/$USER/.config/xfce4/panel/
+	  #mkdir /home/$USER/.config/xfce4/panel/
+          #cd /home/$USER/KickPi-OS/config/
+      	  #unzip -u /home/$USER/KickPi-OS/config/panel.zip
+	  #cp -rf /home/$USER/KickPi-OS/config/panel/* /home/$USER/.config/xfce4/panel/
+	  #cp -rf /home/$USER/KickPi-OS/config/panel.zip /home/$USER/.config/xfce4/panel/
 	 
           sudo chmod -R 777 /home/$USER/.config/xfce4/panel/
 	  fi
