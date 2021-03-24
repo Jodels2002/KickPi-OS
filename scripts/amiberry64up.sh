f7854c12f4c491d76328fef8661f966c3f95d514
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2021-03-24 07:13:23 +0100

    Create amiberry64up.sh

---
+++ scripts/amiberry64up.sh
@@ -0,0 +1,7 @@
+sudo apt-get - y install gir1.2-ibus-1.0 libdbus-1-dev libegl1-mesa-dev libibus-1.0-5 libibus-1.0-dev libice-dev libsm-dev libsndio-dev libwayland-bin libwayland-dev libxi-dev libxinerama-dev libxkbcommon-dev libxrandr-dev libxss-dev libxt-dev libxv-dev x11proto-randr-dev x11proto-scrnsaver-dev x11proto-video-dev x11proto-xinerama-dev
+
+cd
+git clone -b dev https://github.com/midwan/amiberry
+cd amiberry
+#make -j2 PLATFORM=pi64
+make -j2 PLATFORM=pi64-dispmanx
