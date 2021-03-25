d5f1cb4c392acbdaa03856ab107a730d9d4e1395
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2021-03-25 13:09:10 +0100

    Update kick.sh

--- config/kick.sh
+++ config/kick.sh
@@ -1,5 +1,7 @@
+
+omxplayer --no-osd  "/home/$USER/.KickPi-OS/config/splash/Boot.mp4" 
 cd /home/$USER/Amiga/
-omxplayer --no-osd  "/home/$USER/.KickPi-OS/config/splash/Boot.mp4"  && ./amiberry
+./amiberry
      
      
        
