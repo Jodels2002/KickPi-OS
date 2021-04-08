f0c02ea3a08a3890eb77d3574d8d24ef92561d83
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2021-04-08 13:28:56 +0100

    Update pikickme.sh

--- scripts/pikickme.sh
+++ scripts/pikickme.sh
@@ -224,6 +224,8 @@ KickPi-OS_Tools() {
           
       sudo apt install -y mc zip unzip 
       sudo apt install -y rpi-imager
+      sudo apt install -y snapd
+      sudo snap install -y p7zip-desktop
       sudo apt install -y geany geany-plugins-common geany-common xmlstarlet
       sudo apt-get install -y libsdl2-2.0-0 libsdl2-ttf-2.0-0 libsdl2-image-2.0-0 libxml2 flac mpg123 libmpeg2-4    
       sudo apt-get -y install libsdl2-dev libsdl2-ttf-dev libsdl2-image-dev libxml2-dev libflac-dev libmpg123-dev libpng-dev libmpeg2-4-dev
