2b4263ad87cf0b51e3085ed113518938abde92eb
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2021-04-10 10:35:55 +0200

    Update wine.sh

--- scripts/wine.sh
+++ scripts/wine.sh
@@ -5,12 +5,16 @@ sudo apt-key add winehq.key
 sudo dpkg --add-architecture i386 && sudo apt update
 sudo apt -y install \
       wine \
-      wine32 \
+wine msiexec /i ~/Desktop/A      wine32 \
       wine64 \
       libwine \
       libwine:i386 \
       fonts-wine
       
+      
+      cd
+      wget -nc https://download.abime.net/winuae/releases/InstallWinUAE4400_x64.msi
       wine msiexec /i ~/Desktop/AmigaForever.msi
+      wine msiexec /i InstallWinUAE4400_x64.msi
       
 sudo apt -y install coreutils dialog memtest86+ mkisofs genisoimage findutils grub-pc xorriso syslinux os-prober bash passwd sed squashfs-tools live-boot live-config live-config-sysvinit live-boot-initramfs-tools rsync mount laptop-detect util-linux, hwdata
