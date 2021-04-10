22b38158b50d8b030fa217042d688a8f1da30a6d
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2021-04-10 09:40:03 +0200

    Update wine.sh

--- scripts/wine.sh
+++ scripts/wine.sh
@@ -1,3 +1,12 @@
 sudo dpkg --add-architecture i386
 wget -nc https://dl.winehq.org/wine-builds/winehq.key
 sudo apt-key add winehq.key
+
+sudo dpkg --add-architecture i386 && sudo apt update
+sudo apt -y install \
+      wine \
+      wine32 \
+      wine64 \
+      libwine \
+      libwine:i386 \
+      fonts-wine
