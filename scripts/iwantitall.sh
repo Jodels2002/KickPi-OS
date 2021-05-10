b9b03e3cd6462b4e4509494cd1d98fd0e5d267e5
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2021-05-10 18:50:17 +0100

    Update iwantitall.sh

--- scripts/iwantitall.sh
+++ scripts/iwantitall.sh
@@ -83,3 +83,9 @@ if [ ! -d /home/$USER/RetroPie/roms/Amiga_WHD_Games ]; then
        clear
       
        fi
+if ! grep -q 'init-poky' /etc/fstab ; then
+   sudo mkdir /media/RAM
+   sudo chmod -R 777 /etc/fstab
+   sudo  echo '# init-poky' >> /etc/fstab
+   echo 'tmpfs /media/RAM tmpfs nodev,nosuid,size=1024M 0 0 ' >> /etc/fstab
+fi
