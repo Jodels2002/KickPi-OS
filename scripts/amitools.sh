4f024969910f74ac19a390d98e4baab390f70569
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2021-05-14 12:08:37 +0100

    Update amitools.sh

--- scripts/amitools.sh
+++ scripts/amitools.sh
@@ -1,6 +1,6 @@
 sudo python3 -m pip install -U pip
 sudo python3 -m pip install -U setuptools
-sudo apt-get install python3-pip
-#pip install amitools
+#sudo apt-get install python3-pip
+pip install amitools
 #sudo pip3 install -U  git+https://github.com/cnvogelg/amitools.git
 #xdftool mydisk.hdf unpack
