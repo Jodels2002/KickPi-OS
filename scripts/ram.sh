8dbdac4e836a2e14486404e7905ab75f23e2087e
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2021-05-14 08:27:02 +0100

    Delete ram.sh

--- scripts/ram.sh
+++
@@ -1,6 +0,0 @@
-if ! grep -q 'init-poky' /etc/fstab ; then
-   sudo mkdir /media/RAM
-   sudo chmod -R 777 /etc/fstab
-   sudo  echo '# init-poky' >> /etc/fstab
-   echo 'tmpfs /media/RAM tmpfs nodev,nosuid,size=1024M 0 0 ' >> /etc/fstab
-fi
