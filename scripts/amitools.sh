8a845fc70b6dc31cdf493165a55196d9fad03586
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2021-05-26 12:14:08 +0100

    Update amitools.sh

--- scripts/amitools.sh
+++ scripts/amitools.sh
@@ -66,6 +66,7 @@ if [ ! -f /home/$USER/Amiga/Install/ClassicWB_UAE_v28.zip ]; then
       cp -rf /home/pi/.KickPi-OS/Amiga/ClassicWB/Activate /home/pi/Amiga/dir/System_ADVSP/System/S/
       cp -rf /home/pi/.KickPi-OS/Amiga/ClassicWB/ClassicWB-ADVSP.uae /home/pi/Amiga/conf/
       cp -rf /home/pi/Amiga/dir/Software /home/pi/Amiga/dir/System_ADVSP/System/
+      cp -rf /home/pi/Amiga/dir/Games/Kickstarts /home/pi/Amiga/dir/System_ADVSP/System/Devs/Kickstarts
       
       echo " "
       echo " "
@@ -82,7 +83,7 @@ if [ ! -f /home/$USER/Amiga/Install/ClassicWB_UAE_v28.zip ]; then
       cp -rf /home/pi/Amiga/dir/System_ADVSP/System/Temp/* /home/pi/Amiga/dir/System_P96/System/Temp/
       cp -rf /home/pi/.KickPi-OS/Amiga/ClassicWB/ClassicWB-P96.uae /home/pi/Amiga/conf/
       cp -rf /home/pi/Amiga/dir/Software /home/pi/Amiga/dir/System_P96/System/    
-      
+      cp -rf /home/pi/Amiga/dir/Games/Kickstarts /home/pi/Amiga/dir/System_P96/System/Devs/Kickstarts
       #rm -rf /home/pi/Amiga/Install/ClassicWB_UAE_v28/
       
  
