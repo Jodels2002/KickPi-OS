8b3c02193283d2615b3cfd4bbc7a1a2a0bada1f9
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2021-05-28 05:35:19 +0100

    Update amitools.sh

--- scripts/amitools.sh
+++ scripts/amitools.sh
@@ -48,8 +48,8 @@ if [ ! -f /home/$USER/Amiga/Install/ClassicWB_UAE_v28.zip ]; then
       
       fi
       
-      rm -rf  /home/pi/Amiga/dir/System_P96/
-      rm -rf  /home/pi/Amiga/dir/System_ADVSP/
+      sudo rm -rf  /home/pi/Amiga/dir/System_P96/
+      sudo rm -rf  /home/pi/Amiga/dir/System_ADVSP/
       
       
       echo " "
