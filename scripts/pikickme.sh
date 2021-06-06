bf26292016b8fcfcc17a08a652c5d5be16bbcfe2
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2021-06-06 08:45:32 +0100

    Update pikickme.sh

--- scripts/pikickme.sh
+++ scripts/pikickme.sh
@@ -242,7 +242,8 @@ KickPi-OS_Tools() {
       sudo apt install -y geany geany-plugins-common geany-common xmlstarlet
       sudo apt-get install -y libsdl2-2.0-0 libsdl2-ttf-2.0-0 libsdl2-image-2.0-0 libxml2 flac mpg123 libmpeg2-4    
       sudo apt-get -y install libsdl2-dev libsdl2-ttf-dev libsdl2-image-dev libxml2-dev libflac-dev libmpg123-dev libpng-dev libmpeg2-4-dev
-      if [ ! -d /home/$USER/pi-apps/]; then
+     
+     #if [ ! -d /home/$USER/pi-apps/]; then
       cd
       #wget -qO- https://raw.githubusercontent.com/Botspot/pi-apps/master/install | bash
       #else
@@ -250,7 +251,8 @@ KickPi-OS_Tools() {
       #fi
       toilet "KickPi-OS" --metal
       #fi
-      #Some little Amiga stuff....
+     
+     #Some little Amiga stuff....
       sudo apt-get -y install  amiga-fdisk-cross buzztrax grafx2 protracker unadf worker xdms 
       clear
       toilet "KickPi-OS" --metal
