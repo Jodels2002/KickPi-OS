62a31980dfd998a2bd5240c78b129279413f8ff3
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2021-06-08 15:27:56 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -126,7 +126,7 @@ if [ "$(getconf LONG_BIT)" == "64" ]; then
       echo " "
       echo " "
       echo "Raspberry Pi OS 32 bit is running... "
-      echo "rebooting now ...
+      echo "rebooting now ..."
       
       sudo cp -R /home/$USER/KickPi-OS/config/config.txt /boot/config.txt
       sleep 4s
