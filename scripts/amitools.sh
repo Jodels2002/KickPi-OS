8854fc626b94225529a9adae14b0fbcd80271622
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2021-06-20 10:26:49 +0200

    Update amitools.sh

--- scripts/amitools.sh
+++ scripts/amitools.sh
@@ -78,6 +78,7 @@ if [ ! -f /home/$USER/Amiga/Install/ClassicWB_UAE_v28.zip ]; then
       xdftool System_P96.hdf unpack /home/pi/Amiga/dir/System_P96
       cp -rf /home/pi/Amiga/dir/Workbench31/ /home/pi/Amiga/dir/System_P96/System/T/
       cp -rf /home/pi/.KickPi-OS/Amiga/ClassicWB/Startup-Sequence /home/pi/Amiga/dir/System_P96/System/S/
+      cp -rf /home/pi/.KickPi-OS/Amiga/ClassicWB/User-Startup /home/pi/Amiga/dir/System_P96/System/S/
       cp -rf /home/pi/.KickPi-OS/Amiga/ClassicWB/Assign-Startup /home/pi/Amiga/dir/System_P96/System/S/
       cp -rf /home/pi/.KickPi-OS/Amiga/ClassicWB/Activate /home/pi/Amiga/dir/System_P96/System/S/
       
