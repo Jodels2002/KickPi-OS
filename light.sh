e77361346647b5774ae4036df2a4b51290cd8e3f
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2021-09-22 13:12:44 +0200

    Update light.sh

--- light.sh
+++ light.sh
@@ -5,16 +5,16 @@
 
 clear
 echo "Welcome to KickPi-OS"
-echo " "
-echo " "
+echo "Assign >NIL:  Install KickPi-OS"
+echo "Assign >NIL:  B.Titze 2021 "
 
      
       sudo apt -y update
       sudo apt-get install -y toilet dialog mc zip unzip
       clear
       toilet "KickPi-OS" --metal
-     
-	  
+
+mkdir /home/$USER/Amiga   
 sudo chmod -R 777 /home/$USER/Amiga 
 sudo cp -R /home/$USER/KickPi-OS/scripts/* /usr/local/bin	 
 sudo cp -R /boot/config.txt /boot/config_backup.txt
