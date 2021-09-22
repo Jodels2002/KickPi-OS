43f8c3ec281b92ad1ac0b2a7a156f4ac30c5aba3
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2021-09-22 13:16:03 +0200

    Update light.sh

--- light.sh
+++ light.sh
@@ -5,6 +5,7 @@
 
 clear
 echo "Welcome to KickPi-OS"
+echo " "
 echo "Assign >NIL:  Install KickPi-OS"
 echo "Assign >NIL:  B.Titze 2021 "
 
@@ -13,6 +14,9 @@ echo "Assign >NIL:  B.Titze 2021 "
       sudo apt-get install -y toilet dialog mc zip unzip
       clear
       toilet "KickPi-OS" --metal
+
+      echo " "
+      echo "rebooting now ..."
 
 mkdir /home/$USER/Amiga   
 sudo chmod -R 777 /home/$USER/Amiga 
