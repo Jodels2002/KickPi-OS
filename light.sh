e0f404e0eec23dc5a8163b3fbbbec0e1594be79f
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2021-09-22 13:10:32 +0200

    Create light.sh

---
+++ light.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Assign >NIL:  Install KickPi-OS
+# Assign >NIL:  B.Titze 2021
+#******************************************** #startup-sequence  :-) ************* ********************************************
+
+clear
+echo "Welcome to KickPi-OS"
+echo " "
+echo " "
+
+     
+      sudo apt -y update
+      sudo apt-get install -y toilet dialog mc zip unzip
+      clear
+      toilet "KickPi-OS" --metal
+     
+	  
+sudo chmod -R 777 /home/$USER/Amiga 
+sudo cp -R /home/$USER/KickPi-OS/scripts/* /usr/local/bin	 
+sudo cp -R /boot/config.txt /boot/config_backup.txt
+cp  /home/$USER/KickPi-OS/scripts/bashrc /home/$USER/.bashrc
+
+sudo raspi-config nonint do_boot_behaviour B2
+
+
+
+sudo reboot
