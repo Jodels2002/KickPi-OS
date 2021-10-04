64a08190882fbda5fed6d4b04bed0befa7d0effb
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2021-10-04 07:16:19 +0200

    Create deb11.sh

---
+++ scripts/deb11.sh
@@ -0,0 +1,9 @@
+
+
+sudo adduser pi
+sudo usermod pi -a -G pi,adm,dialout,cdrom,sudo,audio,video,plugdev,games,users,input,netdev,spi,i2c,gpio
+sudo visudo
+sudo apt install -y raspberrypi-ui-mods
+
+sudo apt install -y usbmount
+sudo apt install task-xfce-desktop -y
