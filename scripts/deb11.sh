f7ad7f943ddcac6f447de1759c45268fc666cfb7
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2021-10-04 07:23:57 +0200

    Update deb11.sh

--- scripts/deb11.sh
+++ scripts/deb11.sh
@@ -3,7 +3,7 @@
 sudo adduser pi
 sudo usermod pi -a -G pi,adm,dialout,cdrom,sudo,audio,video,plugdev,games,users,input,netdev,spi,i2c,gpio
 sudo visudo
-sudo apt install -y raspberrypi-ui-mods
+sudo apt install -y raspberrypi-ui-mods raspberrypi-sys-mods
 
 sudo apt install -y usbmount
 sudo apt install task-xfce-desktop -y
