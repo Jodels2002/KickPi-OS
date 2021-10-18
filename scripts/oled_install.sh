0bdc969f1d8a085742567cd13ed7717c8dc7aa4f
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2021-10-18 14:34:21 +0100

    Add files via upload

---
+++ scripts/oled_install.sh
@@ -0,0 +1,10 @@
+# https://indibit.de/raspberry-pi-oled-display-128x64-mit-python-ansteuern-i2c/
+
+sudo raspi-config nonint get_i2c
+sudo apt-get -y install python-smbus i2c-tools git python-pil
+git clone https://github.com/BLavery/lib_oled96
+chmod -R 777 lib_oled96
+
+sudo cp -R /home/$USER/KickPi-OS/config/oled.service /etc/systemd/system/oled.service
+sudo systemctl enable oled
+
