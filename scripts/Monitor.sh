4fd3c93d01a46b66856b2e451b182b57afdd53a5
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2021-10-20 06:48:43 +0100

    Add files via upload

---
+++ scripts/Monitor.sh
@@ -0,0 +1,8 @@
+sudo apt-get install python-smbus
+sudo apt-get install i2c-tools
+
+git clone https://github.com/adafruit/Adafruit_Python_SSD1306
+
+sudo chmod -R 777 Adafruit_Python_SSD1306
+cd Adafruit_Python_SSD1306
+sudo python3 setup.py install
