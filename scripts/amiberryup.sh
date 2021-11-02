ab55a80b6f8d495d4cdf5e46ca7c1793e4bc3ed4
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2021-11-02 06:22:36 +0000

    Update amiberryup.sh

--- scripts/amiberryup.sh
+++ scripts/amiberryup.sh
@@ -1,6 +1,6 @@
 
-sudo apt-get install libsdl2-2.0-0 libsdl2-ttf-2.0-0 libsdl2-image-2.0-0 flac mpg123 libmpeg2-4 libraspberrypi-dev
-sudo apt-get install libsdl2-dev libsdl2-ttf-dev libsdl2-image-dev libflac-dev libmpg123-dev libpng-dev libmpeg2-4-dev 
+sudo apt-get install libsdl2-2.0-0 -y libsdl2-ttf-2.0-0 libsdl2-image-2.0-0 flac mpg123 libmpeg2-4 libraspberrypi-dev
+sudo apt-get install libs-y dl2-dev libsdl2-ttf-dev libsdl2-image-dev libflac-dev libmpg123-dev libpng-dev libmpeg2-4-dev 
 sudo apt-get -y install make g++
 
 
