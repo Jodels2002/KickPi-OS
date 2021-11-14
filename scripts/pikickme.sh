260eb38cef6b4bed9185b70029dac953955fb1a4
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2021-11-14 09:05:50 +0100

    Update pikickme.sh

--- scripts/pikickme.sh
+++ scripts/pikickme.sh
@@ -115,23 +115,26 @@ if [ ! -d /OLED/ ]; then
        sudo raspi-config nonint do_i2c 0
       
        
-       
+       sudo apt install -y python3
+       sudo apt-get install -y python3-pip
        sudo apt install -y python-dev
-       sudo apt install -y python-smbus i2c-tools
+       sudo apt install -y python-smbus 
+       sudo apt install -y i2c-tools
        sudo apt install -y python-pil
        sudo apt install -y python-pip
        sudo apt install -y python-setuptools 
        sudo apt install -y python-dev
        
-       #sudo apt-get install -y python3-pip
+       sudo python3 -m pip install -U pip
+       sudo python3 -m pip install -U setuptools       
+       sudo apt-get install -y python3-pip
        #pip3 install adafruit-circuitpython-ssd1306
-       #sudo apt install -y python3
+      
        #sudo chmod -R 777 /home/$USER/Adafruit_Python_SSD1306
        LED_off
        cd /home/pi
-       sudo python -m pip install --upgrade pip setuptools wheel
        sudo pip install Adafruit-SSD1306
-       sudo python -m pip install --upgrade pip setuptools wheel
+       sudo python3 -m pip install --upgrade pip setuptools wheel
    
        sudo cp -rf /home/pi/KickPi-OS/OLED/ /
        sudo cp -rf /home/pi/KickPi-OS/conf/rc.local /etc/
