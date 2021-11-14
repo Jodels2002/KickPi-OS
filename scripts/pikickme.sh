9c7e49433b5cc374497d1d2224002cd7716232d7
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2021-11-14 11:32:11 +0100

    Update pikickme.sh

--- scripts/pikickme.sh
+++ scripts/pikickme.sh
@@ -555,6 +555,9 @@ if [ ! -d /usr/share/themes/Amiga3.x_hidpi/ ]; then
       sudo rm -rf /usr/share/themes/Default/xfwm4/
       sudo cp -rf /usr/share/themes/Amiga3.x_hidpi/* /usr/share/themes/Default/xfwm4/
       sudo cp -rf /home/$USER/KickPi-OS/config/rpd-wallpaper/* /usr/share/backgrounds/
+      sudo cp -rf /home/$USER/KickPi-OS/config/rpd-wallpaper/* /usr/share/images/desktop-base/  
+      sudo cp -rf /home/$USER/KickPi-OS/config/rpd-wallpaper/16bitMemories.png /usr/share/images/desktop-base/desktop-grub.png
+           
       else 
         clear
       	toilet "KickPi-OS" --metal
