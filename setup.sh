d1dbbb3eaa6e9423e8334f07fc60921d24022dcd
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2021-11-15 08:19:05 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -145,9 +145,12 @@ if [ "$(getconf LONG_BIT)" == "64" ]; then
 fi 
 
  sudo cp -R /home/$USER/KickPi-OS/config/splash.service /etc/systemd/system/splash.service
+ sudo cp -R /home/$USER/KickPi-OS/config/splash/Booting.png /etc/systemd/system/Booting.png
  sudo systemctl enable splash
  sudo cp -R /boot/cmdline.txt /home/pi/Documents/cmdline.txt
  sudo chmod -R 777 /home/pi/Documents/cmdline.txt
+ 
+ 
  printf %s " console=tty1 logo.nologo quiet loglevel=0 plymouth.enable=0 vt.global_cursor_default=0 plymouth.ignore-serial-consoles splash fastboot noatime nodiratime noram" >> /home/pi/Documents/cmdline.txt
  sudo cp -Rf  /home/pi/Documents/cmdline.txt /boot/cmdline.txt
 
