cd27410cf2cb0125fb711b9f73a7ab50cfa0d2ed
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2021-11-17 04:52:37 +0000

    Update Pimiga-imp.sh

--- scripts/Pimiga-imp.sh
+++ scripts/Pimiga-imp.sh
@@ -20,13 +20,17 @@ sudo mkdir /home/pi/pimiga/
 sudo mkdir /home/pi/pimiga/disks/
 
 sudo rsync --info=progress2 -auvz /media/pi/rootfs/home/pi/pimiga/disks/ /home/pi/pimiga/disks/
-
+sudo chmod -R 777 /home/pi/pimiga
  
   Pimiga.sh   
-      echo " "
-      echo "       configure files ..."
+      clear
+      echo " copy finished "
+      echo "      ..configure files ..."
       
       sudo chmod -R 777 pimiga 
-      sudo rsync --info=progress2 -auvz /home/pi/pimiga/disks/System/Emulators/Mame0.34/roms/ /home/pi/RetroPie/roms/arcade/
-      sudo rsync --info=progress2 -auvz /home/pi/pimiga/disks/System/Emulators/GnGeo/roms/ /home/pi/RetroPie/roms/neogeo/
-      sudo cp -R -f -v /home/pi/pimiga/disks/System/Emulators/PocketSNES/roms/ /home/pi/RetroPie/roms/mastersystem/
+      echo "      ready ..."
+      
+      
+      # sudo rsync --info=progress2 -auvz /home/pi/pimiga/disks/System/Emulators/Mame0.34/roms/ /home/pi/RetroPie/roms/arcade/
+      # sudo rsync --info=progress2 -auvz /home/pi/pimiga/disks/System/Emulators/GnGeo/roms/ /home/pi/RetroPie/roms/neogeo/
+      # sudo cp -R -f -v /home/pi/pimiga/disks/System/Emulators/PocketSNES/roms/ /home/pi/RetroPie/roms/mastersystem/
