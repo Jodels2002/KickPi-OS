65e82490375b8df2524e0465d688ce9e166f887b
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2021-11-19 11:00:46 +0000

    Create AFimport.sh

---
+++ scripts/AFimport.sh
@@ -0,0 +1,5 @@
+sudo apt install -y msitools
+cd /home/pi/Templates/
+msiextract /home/pi/Desktop/AmigaForever9Plus.msi
+
+cp -r -f "/home/pi/Templates/Program Files/Cloanto/Amiga Forever/Shared/"* /home/pi/Amiga/
