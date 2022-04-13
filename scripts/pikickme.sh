2a5b01de1d3bd49d5a244d4d462d1768d767b7ba
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2022-04-13 08:03:23 +0200

    Update pikickme.sh

--- scripts/pikickme.sh
+++ scripts/pikickme.sh
@@ -184,7 +184,50 @@ if [ "$(getconf LONG_BIT)" == "64" ]; then
       echo " "
       echo "Raspberry Pi OS 32 bit is running... "
       echo ""
-    
+           sudo raspi-config nonint do_i2c 0
+      
+       
+       sudo apt-get install --reinstall python-pip
+       sudo apt install -y python-dev
+       sudo apt install -y python-smbus 
+       sudo apt install -y i2c-tools
+       sudo apt install -y python-pil
+       sudo apt install -y python-pip
+       sudo apt install -y python-setuptools 
+       sudo apt install -y python-dev
+       
+       sudo python -m pip install -U pip
+       sudo python -m pip install -U setuptools       
+       sudo apt-get install -y python-pip
+       #pip3 install adafruit-circuitpython-ssd1306
+      
+       #sudo chmod -R 777 /home/$USER/Adafruit_Python_SSD1306
+       LED_off
+       cd /home/pi
+       sudo pip install Adafruit-SSD1306
+       sudo python3 -m pip install --upgrade pip setuptools wheel
+   
+       sudo cp -rf /home/pi/KickPi-OS/OLED/ /
+       sudo cp -rf /home/pi/KickPi-OS/conf/rc.local /etc/
+       sudo cp -rf /home/pi/KickPi-OS/conf/.bashrc /home/pi/
+       
+   
+       sudo cp -rf /home/pi/KickPi-OS/OLED/ /
+       sudo cp -rf /home/pi/KickPi-OS/conf/rc.local /etc/
+       sudo cp -rf /home/pi/KickPi-OS/conf/.bashrc /home/pi/
+     
+ 
+       
+       sudo chmod -R 777 /OLED/
+       sudo chmod -R 777 /usr/local/bin/
+       sudo chmod -R 777 /etc/rc.local
+       sudo chmod -R 777 /home/pi/.bashrc
+      # (crontab -l 2>/dev/null; echo "*/5 * * * * /usr/local/bin/loop.sh") | crontab -
+      
+       LED
+       if [ -d /OLED/ ]; then
+       KickPi-OS.sh
+       fi
       
 fi    
 
