3425ac7254508e9be5ca05f16956be1d721ab5db
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2025-04-04 06:28:52 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,10 +2,6 @@
 # Assign >NIL:  Install KickPi-OS
 # Assign >NIL:  B.Titze 2021
 
-#******************************************** #todo ************* ********************************************
-#  - Retropie Amiga - rm CDTV7CD32
-#  - https://github.com/jaromaz/MacintoshPi     Basilisk
-#  -  Speed up Cronjobs & more
 #******************************************** #startup-sequence  :-) ************* ********************************************
 
 
@@ -22,61 +18,17 @@ echo " "
      
       sudo apt -y update
       sudo apt-get install -y toilet dialog mc zip unzip wget
-      # sudo apt-get install -y network-*
+   
       sudo apt purge -y lxde  lxde-common lxde-core openbox-lxde-session
       	sudo apt purge -y raspberrypi-ui-mods 
-	# sudo apt purge -y xser* xor* xin*
-      	# sudo apt purge -y gnome*
-       	LED
-	sudo apt-get -y update
-	LED_off
-	sudo apt-get -y upgrade
-	sudo apt -y autoremove
-	LED     
 	sudo apt install -y gnome-com*
         sudo apt install -y ntfs-*
 
       clear
       toilet "KickPi-OS" --metal
      
-      whiptail --msgbox "Optional: \n  \n  Now insert your * AMIGA * USB stick with the \n  * Shared * folder into the Raspberry" 15 50 1
 
 
-
-if    [ ! -d "/media/pi/AMIGA/Shared/" ]; then
-	 clear
-         toilet "KickPi-OS" --metal
-         echo " "
-         echo " "
-	else
-	  clear
-          toilet "KickPi-OS" --metal
-          echo " "
-          echo " "
-	  echo "***  Amiga Forever files found ***"
-	  echo " "
-	  echo "... copy files will take 2-5 min "
-          mkdir /home/$USER/Amiga 
-	  cp -rf /media/pi/AMIGA/Shared/* ~/Amiga  
-	  
-          fi
-
-if    [ ! -d "/media/usb0/Shared/" ]; then
-         clear
-         toilet "KickPi-OS" --metal
-         echo " "
-         echo " "
-	else
-	  clear
-          toilet "KickPi-OS" --metal
-          echo " "
-          echo " "
-          echo "***  Amiga Forever files found ***"
-	  echo " "
-	  echo "... copy files to Amibian will take 2-5 min "
-	  cp -rf /media/usb0/Shared/* ~/Amiga  
-	  
-          fi
 	  
 sudo chmod -R 777 /home/$USER/Amiga 
 sudo cp -R /home/$USER/KickPi-OS/scripts/* /usr/local/bin	 
@@ -84,8 +36,6 @@ sudo cp -R /home/$USER/KickPi-OS/scripts/* /usr/local/bin
 	 
 
 cp  /home/$USER/KickPi-OS/scripts/bashrc /home/$USER/.bashrc
-sudo chmod -R 777 /home/$USER/Amiga   
-
 
  sudo cp -R /home/$USER/KickPi-OS/config/splash.service /etc/systemd/system/splash.service
  sudo cp -R /home/$USER/KickPi-OS/config/splash/Booting.png /etc/systemd/system/Booting.png
