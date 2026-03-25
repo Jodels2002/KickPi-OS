c31339682fc7126ede535c33423c725754969d72
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2026-03-25 13:37:13 +0100

    Update pikickme.sh

--- scripts/pikickme.sh
+++ scripts/pikickme.sh
@@ -157,14 +157,6 @@ python3 -m pip install --break-system-packages adafruit-blinka adafruit-circuitp
     sudo chmod -R 755 /usr/local/bin/
     sudo chmod 644 /home/$USER/.bashrc
 
-    echo "== Cronjob setzen =="
-    sudo  (crontab -l 2>/dev/null; echo "*/5 * * * * /usr/local/bin/loop.sh") | crontab -
-
-
-
-   
-	
-
 
 fi
 
