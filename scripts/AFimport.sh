92e8f50df01d982612c007e6157bdcaabbfbf9f1
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2026-03-28 06:49:18 +0100

    Delete scripts/AFimport.sh

--- scripts/AFimport.sh
+++
@@ -1,5 +0,0 @@
-sudo apt install -y msitools
-cd /home/pi/Templates/
-msiextract /home/pi/Desktop/AmigaForever9Plus.msi
-
-cp -r -f "/home/pi/Templates/Program Files/Cloanto/Amiga Forever/Shared/"* /home/pi/Amiga/
