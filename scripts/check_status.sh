88f30f7b291b440e62d5115669defa18eb06cd21
B.Titze <77006072+Jodels2002@users.noreply.github.com>
2026-03-30 11:36:09 +0200

    Create check_status.sh

---
+++ scripts/check_status.sh
@@ -0,0 +1,18 @@
+cd
+clear
+toilet "KickPi-OS" --metal
+echo "KickPI-OS ROM Operating System and Libraries" 
+echo "Version V11.1 2020-2021 KickPi-OS "
+echo "No Rights Reserved.  "
+echo ""
+echo ""
+echo "1.>  "
+echo "1.>  ( a ) Start Amiga                   *Amiberry*" 
+echo "1.>  ( d ) KickPi Desktop                          " 
+echo "1.>  ( e ) Emulationstation              *RetroPie*" 
+echo "1.>  ( r ) Emulationstation Setup                  "
+echo "1.>  ( u ) KickPi-OS Update                        "
+echo "1.>  ( m ) Boot select                             " 
+echo "1.>  ( c ) Raspi-Config                            "
+echo "1.>  ( s ) Shutdown                                "  
+echo ""
